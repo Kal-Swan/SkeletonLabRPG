4cf4f42584ff4c0744b60fc4fc8e052f01bc7829
Kal-Swan <kal-swan@outlook.com>
2025-10-23 20:33:01 +0100

    added logging

--- web/main/src/routes/+layout.svelte
+++ web/main/src/routes/+layout.svelte
@@ -5,11 +5,17 @@
 	import { get } from 'svelte/store';
 
 	let { children, data } = $props();
+	console.log('Layout root data:');
+	console.log(data);
 	configStore.set(data.config!);
 	$effect(() => {
+		console.log('effect in +layout.svelte - sync access token to cookieStore');
+		console.log($accessTokenStore);
 		if ($accessTokenStore) {
+			console.log('setting auth_token cookie');
 			cookieStore.set('auth_token', $accessTokenStore);
 		} else {
+			console.log('deleting auth_token cookie');
 			cookieStore.delete('auth_token');
 		}
 	});
