015e81bf9b33ea4ef6c09f579141577eb83ee795
Kal-Swan <kal-swan@outlook.com>
2026-01-01 15:25:26 +0000

    lower cased all in getall endpoint (#68)

--- web/main/src/routes/(protected)/rpgbuild/endpoint.ts
+++ web/main/src/routes/(protected)/rpgbuild/endpoint.ts
@@ -1,6 +1,6 @@
 import { baseApiEndpoint } from '@environment/urls';
 
 export const baseBuildsEndpoint = `${baseApiEndpoint}/build`;
-export const getAllRpgBuildsEndpoint = `${baseBuildsEndpoint}/getAll`;
+export const getAllRpgBuildsEndpoint = `${baseBuildsEndpoint}/getall`;
 export const createRpgBuildEndpoint = `${baseBuildsEndpoint}/create`;
 export const updateRpgBuildEndpoint = `${baseBuildsEndpoint}`;
